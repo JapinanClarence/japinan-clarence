f1ecf8fd1405507f81e5e50542042b722e324cf6
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2025-02-04 10:50:29 +0800

    fixed tracing beam

--- src/App.jsx
+++ src/App.jsx
@@ -168,157 +168,161 @@ function App() {
       </header>
 
       <div className="relative pt-20 md:px-20 lg:px-[30rem]">
-        <div
-          className="h-[40rem] md:h-[60rem] p-10 flex flex-col lg:flex-row justify-center lg:justify-between gap-10 items-center"
-          id="home"
-        >
-          <div className="">
-            <h1 className="relative z-10 text-4xl lg:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-start font-sans font-bold">
-              JapinanClarence
-            </h1>
+        <TracingBeam>
+          <div
+            className="h-[40rem] md:h-[60rem] p-10 flex flex-col lg:flex-row justify-center lg:justify-between gap-10 items-center"
+            id="home"
+          >
+            <div className="">
+              <h1 className="relative z-10 text-4xl lg:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-start font-sans font-bold">
+                JapinanClarence
+              </h1>
 
-            <div className="text-white md:text-2xl">
-              I build
-              <FlipWords words={words} className={"text-white md:text-2xl"} />
-              website
+              <div className="text-white md:text-2xl">
+                I build
+                <FlipWords words={words} className={"text-white md:text-2xl"} />
+                websites
+              </div>
+            </div>
+            <div className="aspect-square h-[300px] overflow-clip z-10">
+              <img src={heroImage} alt="" className=" object-cover" />
             </div>
           </div>
-          <div className="aspect-square h-[300px] overflow-clip z-10">
-            <img src={heroImage} alt="" className=" object-cover" />
-          </div>
-        </div>
-        <div className="space-y-10">
-          <div className="text-white px-10 md:px-0 py-20" id="about">
-            <h1 className="text-center text-4xl mb-5">A Bit About Me</h1>
-            <p className="text-pretty">
-              I am a 4th-year Bachelor of Science in Information Technology
-              student with a strong passion for backend web development. I have
-              experience working with PHP, MySQL, and the MERN stack and am
-              proficient in building scalable and efficient RESTful APIs.
-              Constantly eager to learn and improve, I enjoy optimizing database
-              structures, enhancing system performance, and developing secure,
-              high-quality web applications.
-            </p>
-          </div>
-          <div className="text-white px-10 md:px-0 py-10">
-            <h1 className="text-center text-4xl font-medium">Tech Stack</h1>
-            <div className="mt-10 grid gap-5 md:grid-flow-col grid-cols-4 md:grid-cols-9">
-              <img src={jsIcon} alt="" />
-              <img src={phpIcon} alt="" />
-              <img src={nodeJsIcon} alt="" />
-              <img src={expressJsIcon} alt="" />
-              <img src={reactjsIcon} alt="" />
-              <img src={bootstrapIcon} alt="" />
-              <img src={tailwindIcon} alt="" />
-              <img src={mysqslIcon} alt="" />
-              <img src={mongodbIcon} alt="" />
+          <div className="space-y-10">
+            <div className="text-white px-10 md:px-0 py-20" id="about">
+              <h1 className="text-center text-4xl mb-5">A Bit About Me</h1>
+              <p className="text-pretty">
+                I am a 4th-year Bachelor of Science in Information Technology
+                student with a strong passion for backend web development. I
+                have experience working with PHP, MySQL, and the MERN stack and
+                am proficient in building scalable and efficient RESTful APIs.
+                Constantly eager to learn and improve, I enjoy optimizing
+                database structures, enhancing system performance, and
+                developing secure, high-quality web applications.
+              </p>
             </div>
-          </div>
-          <div className="text-white px-10 md:px-0 py-20" id="projects">
-            <h1 className="text-center text-4xl font-medium mb-5">Projects</h1>
-            <h2>Here are some of the projects I have worked on:</h2>
-            <div className="mt-10 grid md:grid-flow-row grid-rows-1 md:grid-cols-2  gap-5">
-              <Card className="h-full overflow-hidden">
-                <CardHeader className="p-0">
-                  <div className={`h-56 bg-slate-200`}>
-                    <img
-                      src={transitMaster}
-                      alt=""
-                      className="h-full w-full object-cover"
-                    />
-                  </div>
-                </CardHeader>
-                <CardContent className="p-5">
-                  <CardTitle className="text-md">
-                    {"TransitMaster"}
-                    <Button
-                      className="p-0 ml-2 size-2"
-                      variant="icon"
-                      onClick={() =>
-                        window.open(
-                          "https://app-transitmaster.vercel.app/",
-                          "_blank"
-                        )
-                      }
-                    >
-                      <Link size={5} />
-                    </Button>
-                  </CardTitle>
-                  <CardDescription>
-                    {"School vehicle scheduling and tracking system"}
-                  </CardDescription>
-                </CardContent>
-              </Card>
+            <div className="text-white px-10 md:px-0 py-10">
+              <h1 className="text-center text-4xl font-medium">Tech Stack</h1>
+              <div className="mt-10 grid gap-5 md:grid-flow-col grid-cols-4 md:grid-cols-9">
+                <img src={jsIcon} alt="" />
+                <img src={phpIcon} alt="" />
+                <img src={nodeJsIcon} alt="" />
+                <img src={expressJsIcon} alt="" />
+                <img src={reactjsIcon} alt="" />
+                <img src={bootstrapIcon} alt="" />
+                <img src={tailwindIcon} alt="" />
+                <img src={mysqslIcon} alt="" />
+                <img src={mongodbIcon} alt="" />
+              </div>
+            </div>
+            <div className="text-white px-10 md:px-0 py-20" id="projects">
+              <h1 className="text-center text-4xl font-medium mb-5">
+                Projects
+              </h1>
+              <h2>Here are some of the projects I have worked on:</h2>
+              <div className="mt-10 grid md:grid-flow-row grid-rows-1 md:grid-cols-2  gap-5">
+                <Card className="h-full overflow-hidden">
+                  <CardHeader className="p-0">
+                    <div className={`h-56 bg-slate-200`}>
+                      <img
+                        src={transitMaster}
+                        alt=""
+                        className="h-full w-full object-cover"
+                      />
+                    </div>
+                  </CardHeader>
+                  <CardContent className="p-5">
+                    <CardTitle className="text-md">
+                      {"TransitMaster"}
+                      <Button
+                        className="p-0 ml-2 size-2"
+                        variant="icon"
+                        onClick={() =>
+                          window.open(
+                            "https://app-transitmaster.vercel.app/",
+                            "_blank"
+                          )
+                        }
+                      >
+                        <Link size={5} />
+                      </Button>
+                    </CardTitle>
+                    <CardDescription>
+                      {"School vehicle scheduling and tracking system"}
+                    </CardDescription>
+                  </CardContent>
+                </Card>
 
-              <Card className="h-full overflow-hidden">
-                <CardHeader className="p-0 ">
-                  <div className={`h-56 bg-slate-200 overflow-clip`}>
-                    <img
-                      src={aceTrack}
-                      alt=""
-                      className="h-full w-full object-cover"
-                    />
-                  </div>
-                </CardHeader>
-                <CardContent className="p-5">
-                  <CardTitle className="text-md flex items-center">
-                    {"AceTrack"}
-                    <Button
-                      className="p-0 ml-2 size-2"
-                      variant="icon"
-                      onClick={() =>
-                        window.open(
-                          "https://acetrack.vercel.app/home",
-                          "_blank"
-                        )
+                <Card className="h-full overflow-hidden">
+                  <CardHeader className="p-0 ">
+                    <div className={`h-56 bg-slate-200 overflow-clip`}>
+                      <img
+                        src={aceTrack}
+                        alt=""
+                        className="h-full w-full object-cover"
+                      />
+                    </div>
+                  </CardHeader>
+                  <CardContent className="p-5">
+                    <CardTitle className="text-md flex items-center">
+                      {"AceTrack"}
+                      <Button
+                        className="p-0 ml-2 size-2"
+                        variant="icon"
+                        onClick={() =>
+                          window.open(
+                            "https://acetrack.vercel.app/home",
+                            "_blank"
+                          )
+                        }
+                      >
+                        <Link size={5} />
+                      </Button>
+                    </CardTitle>
+                    <CardDescription>
+                      {
+                        "Efficient scanning and clear identification for seamless attendance management."
                       }
-                    >
-                      <Link size={5} />
-                    </Button>
-                  </CardTitle>
-                  <CardDescription>
-                    {
-                      "Efficient scanning and clear identification for seamless attendance management."
-                    }
-                  </CardDescription>
-                </CardContent>
-              </Card>
-              <Card className="h-full overflow-hidden">
-                <CardHeader className="p-0 ">
-                  <div className={`h-56 bg-slate-200 overflow-clip`}>
-                    <img
-                      src={mangoDisease}
-                      alt=""
-                      className="h-full w-full object-cover"
-                    />
-                  </div>
-                </CardHeader>
-                <CardContent className="p-5">
-                  <CardTitle className="text-md flex items-center">
-                    {"MangoDisease Classification Syst"}
-                    <Button
-                      className="p-0 ml-2 size-2"
-                      variant="icon"
-                      onClick={() =>
-                        window.open(
-                          "https://github.com/JapinanClarence/Mango-Desease-Classification-Web-App.git",
-                          "_blank"
-                        )
+                    </CardDescription>
+                  </CardContent>
+                </Card>
+                <Card className="h-full overflow-hidden">
+                  <CardHeader className="p-0 ">
+                    <div className={`h-56 bg-slate-200 overflow-clip`}>
+                      <img
+                        src={mangoDisease}
+                        alt=""
+                        className="h-full w-full object-cover"
+                      />
+                    </div>
+                  </CardHeader>
+                  <CardContent className="p-5">
+                    <CardTitle className="text-md flex items-center">
+                      {"MangoDisease Classification Syst"}
+                      <Button
+                        className="p-0 ml-2 size-2"
+                        variant="icon"
+                        onClick={() =>
+                          window.open(
+                            "https://github.com/JapinanClarence/Mango-Desease-Classification-Web-App.git",
+                            "_blank"
+                          )
+                        }
+                      >
+                        <Github size={5} />
+                      </Button>
+                    </CardTitle>
+                    <CardDescription>
+                      {
+                        "A mango disease classification system, made with tensorflow."
                       }
-                    >
-                      <Github size={5} />
-                    </Button>
-                  </CardTitle>
-                  <CardDescription>
-                    {
-                      "A mango disease classification system, made with tensorflow."
-                    }
-                  </CardDescription>
-                </CardContent>
-              </Card>
+                    </CardDescription>
+                  </CardContent>
+                </Card>
+              </div>
             </div>
           </div>
-        </div>
+        </TracingBeam>
       </div>
 
       <BackgroundBeams className="hidden md:flex" />
