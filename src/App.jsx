910efcf3af72f53b4a1decc4cb743a8d15502539
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2026-03-18 22:29:07 +0800

    ✨ implemented vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Footer } from "./components/Footer";
 import {NavMain} from "./components/NavMain";
 import { NavMobile } from "./components/NavMobile";
 import Home from "./pages";
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
 
   return (
     <div className="dark:bg-neutral-950">
+      <Analytics/>
       <NavMain/>
       <NavMobile/>
       <Home/>
