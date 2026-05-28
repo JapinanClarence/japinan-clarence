95d8e2fd51855387f4c13be00a7f3af918ff1b9b
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2026-05-28 16:23:17 +0800

    Update resume link to Google Docs

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -59,7 +59,7 @@ export default function Home() {
               >
                 Get in Touch
               </Button>
-              <a href="/JapinanResume.pdf">
+              <a href="https://docs.google.com/document/d/10b-B5XwK_Io6qiyzOAsD3oY1cG-GkxpP/edit?usp=sharing&ouid=100613609772722422706&rtpof=true&sd=true">
                 <Button variant={isDark ? "secondary" : "outline"} size="sm">
                   <FileText />
                   Resume
